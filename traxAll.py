ad4a72876675a4d4c7d2490b642379452b0b1588
unknown <maximos@uoguelph.ca>
2022-12-20 00:23:15 -0500

    Working grid, WIP add

--- traxAll.py
+++ traxAll.py
@@ -1,29 +1,100 @@
 from kivy.metrics import dp
 
-from kivymd.app import App
-from kivymd.uix.datatables import DataTable
-from kivymd.uix.screen import Screen
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.menu import MDDropdownMenu
 
 import csv
 from datetime import date
 
 
-class TraxAll(App):
+class TraxAll(MDApp):
     data = []
+    dataTable = None
+    columnToSortBy = 0
+    screen = MDScreen()
 
     def build(self):
-        self.addToList ("csvFiles/december2021.csv")
-        print(self.data)
-        self.dataTable = DataTable (
+        self.listFromFile ("csvFiles/transactions.csv")
+
+        self.dataTable = MDDataTable (
+            pos_hint = {"center_x" : 0.5, "center_y" : 0.5},
+            size_hint = (1.0, 0.85),
             check = False,
-            column_data = [("Date", dp(20)), ("Cost", dp(20)), ("Store/Service", dp(30)), ("Category", dp(30)), ("Description", dp(30)), ("Payment Method", dp(30))],
+            column_data = [("Date", dp(20)), ("Cost", dp(20)), ("Store/Service", dp(30)), ("Category", dp(30)), ("Payment Method", dp(30)), ("Description", dp(30))],
             row_data = self.data,
-            rows_num = 1000
+            rows_num = 1000,
+            sorted_on = "Date",
         )
 
-        screen = Screen()
-        screen.add_widget(self.dataTable)
-        return screen
+        addTransactionButtonBox = MDBoxLayout(
+            pos_hint = {"center_x" : 0.5},
+            adaptive_size = True,
+            padding = "10dp",
+            spacing = "10dp"
+        )
+
+        addTransactionButton = MDFillRoundFlatButton(
+            text = "Add Transaction",
+            on_release = lambda x: self.askForNewTransaction()
+        )
+
+        layout = MDFloatLayout()
+        
+        addTransactionButtonBox.add_widget(addTransactionButton)
+        self.screen.add_widget(self.dataTable)
+        self.screen.add_widget(addTransactionButtonBox)
+        self.addToList((date.fromisoformat("2022-03-11"),"This","os ","a","test","stub"))
+        return self.screen
+
+    @staticmethod
+    def askForNewTransaction():
+        datePicker = MDDatePicker(
+            on_cancel = lambda x: print("Daniel")
+        )
+
+        dialog = MDDialog(
+            title = "Add Transaction",
+            type = "custom",
+            content_cls = MDBoxLayout(
+                MDTextField(
+                    hint_text = "Cost",
+                    input_filter = "float",
+                ),
+                MDTextField(
+                    hint_text = "Store/Service",
+                ),
+                MDTextField(
+                    hint_text = "Description"
+                ),
+                orientation = "vertical",
+                spacing = "12dp",
+                size_hint_y = None,
+                height = "400dp"
+            ),
+            buttons = [
+                MDFlatButton(
+                    text = "CANCEL",
+                    on_release = lambda x : dialog.dismiss()
+                ),
+                MDFillRoundFlatButton(
+                    text = "ADD"
+                )
+            ]
+        )
+
+        dialog.open()
+        datePicker.open()
+
+
 
     @staticmethod
     def readFromFile(filename):
@@ -31,13 +102,30 @@ class TraxAll(App):
         csvFileReader = csv.reader(csvFilename)
         return csvFileReader
 
-    def addToList(self, filename):
+    def listFromFile(self, filename):
         try:
             fileReader = self.readFromFile(filename)
         except IOError as err:
-            return -1
+            print(filename)
+            return
 
         for rowData in fileReader:
-            self.data.append((int (rowData[0]), rowData[1], rowData[2], rowData[3], rowData[4], rowData[5]))
+            self.data.append((date.fromisoformat(rowData[0]), rowData[1], rowData[2], rowData[3], rowData[4], rowData[5]))
+
+        self.data.sort(key = lambda data: data[0], reverse = True)
+
+    def saveToFile(self, filename):
+        csvFilename = open(filename, 'w', newline = '')
+        csvWriter = csv.writer(csvFilename)
+        
+        for rowData in self.dataTable.row_data:
+            csvWriter.writerow(rowData)
+
+    def addToList(self, newData):
+        self.dataTable.add_row(newData)
+        self.dataTable.row_data.sort(key = lambda data: data[0], reverse = True)
+
+        self.saveToFile("csvFiles/transactions.csv")
+
 
 TraxAll().run()
