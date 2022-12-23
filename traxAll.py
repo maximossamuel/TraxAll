42d374a975dcf117e444dd9126548b256bea281d
unknown <maximos@uoguelph.ca>
2022-12-23 18:47:22 -0500

    WIP calculate function

--- traxAll.py
+++ traxAll.py
@@ -7,6 +7,7 @@ from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFillRoundFlatButton
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.button import MDFloatingActionButton
 from kivymd.uix.button import MDFloatingActionButtonSpeedDial
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.textfield import MDTextField
@@ -70,24 +71,30 @@ class TraxAll(MDApp):
             sorted_on = "Date",
         )
 
-        self.dateLabel = MDLabel()
+        # self.dateLabel = MDLabel()
 
-        addTransactionButtonBox = MDBoxLayout(
-            pos_hint = {"center_x" : 0.5},
-            adaptive_size = True,
-            padding = "10dp",
-            spacing = "10dp"
-        )
+        # addTransactionButtonBox = MDBoxLayout(
+        #     pos_hint = {"center_x" : 0.5},
+        #     adaptive_size = True,
+        #     padding = "10dp",
+        #     spacing = "10dp"
+        # )
 
-        addTransactionButton = MDFillRoundFlatButton(
-            text = "Add Transaction",
-            on_release = lambda x: self.askForNewTransaction()
-        )
+        # addTransactionButton = MDFillRoundFlatButton(
+        #     text = "Add Transaction",
+        #     on_release = lambda x: self.askForNewTransaction()
+        # )
 
         addButton = MDFloatingActionButtonSpeedDial(
             data = dialOptions,
             root_button_anim = True,
-            pos_hint = {"center_x" : 0.5}
+            pos_hint = {"center_x" : 0.2}
+        )
+
+        calculateButton = MDFloatingActionButton(
+            icon = "icons/reciept.png",
+            on_release = lambda x : self.askForCalculationInfo(),
+            #pos_hint = {"center_x" : 0.8}
         )
 
 
@@ -95,13 +102,19 @@ class TraxAll(MDApp):
         
         #addTransactionButtonBox.add_widget(addButton)
         self.screen.add_widget(self.dataTable)
-        self.screen.add_widget(addButton)
-        self.addTransaction((date.fromisoformat("2022-03-11"),"This","os ","a","test","stub"))
+        self.screen.add_widget(
+            MDBoxLayout(
+                addButton,
+                calculateButton,
+                padding = "20dp",
+                pos_hint = {"center_x" : -0.36}
+            )
+        )
+        #self.addTransaction((date.fromisoformat("2022-03-11"),"This","os ","a","test","stub"))
         return self.screen
 
     def askForNewTransaction(self):
-
-        transaction = []
+        dropDownItems = []
 
         # self.dateLabel.text = "Date"
 
@@ -119,11 +132,30 @@ class TraxAll(MDApp):
         # datePicker = MDDatePicker()
         # datePicker.on_save = self.on_save and datePicker.dismiss
 
+        dateTextField = MDTextField(
+            hint_text = "Date",
+            validator = "date",
+            date_format = "yyyy/mm/dd",
+            helper_text = "YYYY/MM/DD Format",
+            required = True,
+            text = str(datetime.now().year) + "/" + str(datetime.now().month) + "/" + str(datetime.now().day)
+        )
+
+        costTextField = MDTextField(
+            hint_text = "Cost",
+            input_filter = "float",
+            required = True
+        )
+
+        descriptionTextField = MDTextField(
+            hint_text = "Description"
+        )
+
         paymentMethodListItems = [
             {
                 "text" : item[0],
                 "viewclass" : "OneLineListItem",
-                "on_release" : lambda x = item : [paymentMethodDropItem.set_item(x[0]), paymentMethodMenu.dismiss()]
+                "on_release" : lambda x = item : [dropDownItems.append(["P", x[0]]), paymentMethodDropItem.set_item(x[0]), paymentMethodMenu.dismiss()]
             } for item in self.paymentMethods
         ]
 
@@ -131,7 +163,7 @@ class TraxAll(MDApp):
             {
                 "text" : item[0],
                 "viewclass" : "OneLineListItem",
-                "on_release" : lambda x = item : [vendorDropItem.set_item(x[0]), vendorMenu.dismiss()]
+                "on_release" : lambda x = item : [dropDownItems.append(["V", x[0]]), vendorDropItem.set_item(x[0]), vendorMenu.dismiss()]
             } for item in self.vendors
         ]
 
@@ -139,7 +171,7 @@ class TraxAll(MDApp):
             {
                 "text" : item [0],
                 "viewclass" : "OneLineListItem",
-                "on_release" : lambda x = item : [categoryDropItem.set_item(x[0]), categoryMenu.dismiss()] 
+                "on_release" : lambda x = item : [dropDownItems.append(["C", x[0]]), categoryDropItem.set_item(x[0]), categoryMenu.dismiss()] 
             } for item in self.categories
         ]
 
@@ -180,19 +212,8 @@ class TraxAll(MDApp):
             title = "Add Transaction",
             type = "custom",
             content_cls = MDBoxLayout(
-                MDTextField(
-                    hint_text = "Date",
-                    validator = "date",
-                    date_format = "yyyy/mm/dd",
-                    helper_text = "YYYY/MM/DD Format",
-                    required = True,
-                    text = str(datetime.now().year) + "/" + str(datetime.now().month) + "/" + str(datetime.now().day)
-                ),
-                MDTextField(
-                    hint_text = "Cost",
-                    input_filter = "float",
-                    required = True
-                ),
+                dateTextField,
+                costTextField,
                 MDBoxLayout(
                     MDLabel(
                         text = "Vendor/Service"
@@ -214,9 +235,7 @@ class TraxAll(MDApp):
                     paymentMethodDropItem,
                     padding = "12dp"
                 ),                
-                MDTextField(
-                    hint_text = "Description"
-                ),
+                descriptionTextField,
                 orientation = "vertical",
                 spacing = "12dp",
                 size_hint_y = None,
@@ -229,7 +248,7 @@ class TraxAll(MDApp):
                 ),
                 MDFillRoundFlatButton(
                     text = "ADD",
-                    on_release = lambda x : dialog.dismiss() if self.addTransaction() else errorDialog.open()   
+                    on_release = lambda x : dialog.dismiss() if self.addTransaction([dateTextField.text, costTextField.text, dropDownItems, descriptionTextField.text]) else errorDialog.open()   
                 )
             ]
         )
@@ -279,6 +298,149 @@ class TraxAll(MDApp):
 
         dialog.open()
 
+    def askForCalculationInfo(self):
+    
+        errorDialog = MDDialog(
+            title = "Error",
+            text = "Something's not right. Please review your inputs and try again.",
+            buttons = [
+                MDFillRoundFlatButton(
+                    text = "OK",
+                    on_release = lambda x : errorDialog.dismiss()
+                )
+            ]
+        )
+
+        startDateTextField = MDTextField(
+            hint_text = "Start Date",
+            validator = "date",
+            date_format = "yyyy/mm/dd",
+            helper_text = "YYYY/MM/DD Format",
+            required = True,
+            text = str(datetime.now().year) + "/" + str(datetime.now().month) + "/" + str(datetime.now().day)
+        )
+
+        endDateTextField = MDTextField(
+            hint_text = "End Date",
+            validator = "date",
+            date_format = "yyyy/mm/dd",
+            helper_text = "YYYY/MM/DD Format",
+            required = True,
+            text = str(datetime.now().year) + "/" + str(datetime.now().month) + "/" + str(datetime.now().day)
+        )
+
+        dropDownItems = []
+
+        paymentMethodListItems = [
+            {
+                "text" : item[0],
+                "viewclass" : "OneLineListItem",
+                "on_release" : lambda x = item : [dropDownItems.append(["P", x[0]]), paymentMethodDropItem.set_item(x[0]), paymentMethodMenu.dismiss()]
+            } for item in self.paymentMethods
+        ]
+
+        vendorListItems = [
+            {
+                "text" : item[0],
+                "viewclass" : "OneLineListItem",
+                "on_release" : lambda x = item : [dropDownItems.append(["V", x[0]]), vendorDropItem.set_item(x[0]), vendorMenu.dismiss()]
+            } for item in self.vendors
+        ]
+
+        categoryListItems = [
+            {
+                "text" : item [0],
+                "viewclass" : "OneLineListItem",
+                "on_release" : lambda x = item : [dropDownItems.append(["C", x[0]]), categoryDropItem.set_item(x[0]), categoryMenu.dismiss()] 
+            } for item in self.categories
+        ]
+
+        vendorDropItem = MDDropDownItem()
+        paymentMethodDropItem = MDDropDownItem()
+        categoryDropItem = MDDropDownItem()
+
+        vendorMenu = MDDropdownMenu(
+            items = vendorListItems,
+            position = "center",
+            caller = vendorDropItem,
+            width_mult = 4
+        )
+
+        paymentMethodMenu = MDDropdownMenu(
+            items = paymentMethodListItems,
+            position = "center",
+            caller = paymentMethodDropItem,
+            width_mult = 4
+        )
+
+        categoryMenu = MDDropdownMenu(
+            items = categoryListItems,
+            position = "center",
+            caller = categoryDropItem,
+            width_mult = 4
+        )
+
+        vendorDropItem.text = "All"
+        paymentMethodDropItem.text = "All"
+        categoryDropItem.text = "All"
+
+        vendorDropItem.on_release = vendorMenu.open
+        paymentMethodDropItem.on_release = paymentMethodMenu.open
+        categoryDropItem.on_release = categoryMenu.open
+
+        dialog = MDDialog(
+            title = "Calculate Expenses",
+            type = "custom",
+            content_cls = MDBoxLayout(
+                MDBoxLayout(
+                    startDateTextField,
+                    endDateTextField,
+                    spacing = "30dp"
+                ),
+                MDBoxLayout(
+                    MDLabel(
+                        text = "Vendor/Service"
+                    ),
+                    vendorDropItem,
+                    padding = "12dp"
+                ),
+                MDBoxLayout(
+                    MDLabel(
+                        text = "Category"
+                    ),
+                    categoryDropItem,
+                    padding = "12dp"
+                ),
+                MDBoxLayout(
+                    MDLabel(
+                        text = "Payment Method"
+                    ),
+                    paymentMethodDropItem,
+                    padding = "12dp"
+                ),                
+                orientation = "vertical",
+                size_hint_y = None,
+                spacing = "12dp",
+                height = "250dp"
+            ),
+            buttons = [
+                MDFlatButton(
+                    text = "CANCEL",
+                    on_release = lambda x : dialog.dismiss()
+                ),
+                MDFillRoundFlatButton(
+                    text = "CALCULATE",
+                    on_release = lambda x : dialog.dismiss() if self.showResults(startDateTextField.text, endDateTextField, dropDownItems) else errorDialog.open()   
+                )
+            ]
+        )
+
+        dialog.open()
+
+    @staticmethod
+    def showResults(self, startDateString, endDateString, selections):
+        return False
+
     @staticmethod
     def readFromFile(filename):
         csvFilename = open(filename, encoding="utf-8-sig")
@@ -316,7 +478,40 @@ class TraxAll(MDApp):
         csvWriter.writerows(itemList)
 
     def addTransaction(self, newData):
-        self.dataTable.add_row(newData)
+        newTransaction = []
+        
+        newTransaction.append(date.fromisoformat(newData[0].replace('/', '-')))
+
+        if newTransaction[0] > date.today():
+            print ("Date")
+            return False
+        
+        newTransaction.append("$" + newData[1])
+
+        if newTransaction[1] == "$":
+            print("Money")
+            return False
+
+        for i in range (0, 3):
+            newTransaction.append(None)
+
+        for i in range (0, len(newData[2])):
+            if (newData[2][i][0] == "P"):
+                newTransaction[4] = newData[2][i][1]
+            
+            elif (newData[2][i][0] == "C"):
+                newTransaction[3] = newData[2][i][1]
+
+            elif (newData[2][i][0] == "V"):
+                newTransaction[2] = newData[2][i][1]
+
+        for i in range(2, 5):
+            if newTransaction[i] == None:
+                return False
+
+        newTransaction.append(newData[3])
+        
+        self.dataTable.add_row(newTransaction)
         self.dataTable.row_data.sort(key = lambda data: data[0], reverse = True)
 
         self.saveToFile(self.dataTable.row_data, self.transactionsFile)
@@ -335,6 +530,5 @@ class TraxAll(MDApp):
         self.saveToFile(itemList, filename)
         
         return True
-
 
 TraxAll().run()
